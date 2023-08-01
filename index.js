a0e644f0d63f31f1e4402867b2d80ff9edb0f677
Khalid G <kgibson4795@gmail.com>
2023-08-01 00:59:11 -0400

    created questions and buttons that function and progress

---
+++ index.js
@@ -0,0 +1,72 @@
+
+
+let quizDiv = document.querySelector("#quiz");
+let startDiv = document.querySelector("#start");
+let questionButton1 = document.querySelector("#answer1")
+let questionButton2 = document.querySelector("#answer2")
+let questionButton3 = document.querySelector("#answer3")
+let questionButton4 = document.querySelector("#answer4")
+
+// let highScores = JSON.parse(localStorage.getItem("highScores")) || []
+
+let timer = 90
+let h4El = document.querySelector("h4")
+let timerId = setInterval(function(){
+
+    h4El.textContent = timer
+timer -=1
+console.log(timer);
+
+}, 1000)
+
+
+let questions = [{question:"Which pokemon does Ash get at the start of pokemon?", answers:["Charmander","Squirtle", "Bulbasaur", "Pikachu"], correctAnswer: "Pikachu"},
+                 {question:"Which pokemon deals fire damage?", answers:["Squirtle", "Charmander", "Pikachu", "Bulbasaur"], correctAnswer: "Charmander"},
+                 {question:"Which pokemon has a high damaging attack called water pulse?", answers:["Pikachu", "Bulbasaur", "Squirtle", "Charmander"], correctAnswer: "Squirtle"},
+                 {question:"Which pokemon has a flower on its back that grows as it evolves?", answers:["Bulbasaur", "Charmander", "Pikachu", "Squirtle"], correctAnswer: "Bulbasaur"},
+                 {question:"What is Pokemon's most famous catch phrase?", answers:["I need more", "Gotta win next time", "I choose you Boots", "Gotta catch em all"], correctAnswer: "Gotta catch em all"}
+
+]
+
+
+let currentQuestion = 0
+
+renderQuestion();
+
+function renderQuestion(){
+
+
+    
+    console.log(questions[currentQuestion].question)
+    questionButton1.textContent = questions[currentQuestion].answers[0];
+    questionButton2.textContent = questions[currentQuestion].answers[1];
+    questionButton3.textContent = questions[currentQuestion].answers[2];
+    questionButton4.textContent = questions[currentQuestion].answers[3];
+    console.log("correct answer:" + questions[currentQuestion].correctAnswer)
+}
+
+
+
+quizDiv.addEventListener("click", function(event){
+
+    if(event.target.matches("button")){
+
+            console.log("clicked!")
+            console.log("value:" + event.target.innerText);
+            console.log("correct answer:" + questions[currentQuestion].correctAnswer);
+            
+            currentQuestion++
+            renderQuestion();
+
+    }
+})
+
+
+
+
+
+
+
+//some button that saves
+
+// localStorage.setItem("highScores", JSON.stringify([{"initials": "dre", "score": 80}]))
